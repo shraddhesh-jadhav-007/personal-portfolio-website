c943ee03ce298ecb2b04286e051257f0d4eb4b53
shraddhesh-jadhav-007 <shraddhesh.jadhav.07@gmail.com>
2025-09-09 11:08:49 +0530

    Old Netlify link replaced

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ First Project using React + Tailwind + DaisyUI
 
 # Updated ProjectList
 Provided link of Project-repository, and live-link of Netlify
+Old link was not working. So, replaced with new link
--- src/pages/ProjectsList.js
+++ src/pages/ProjectsList.js
@@ -8,7 +8,7 @@ export const ProjectsList = [
     title: "Portfolio",
     description:
      ( "Developed a dynamic, fully responsive Portfolio Website with Mobile-first design, animations and interactive features using React hooks, and libraries like React Spring & AOS for animations. [ Tech Stack: React, Tailwind CSS, DaisyUI, CSS ]"),
-    liveLink: "personal-portfoliopp.netlify.app",
+    liveLink: "https://personal-portfoliopp.netlify.app/",
     CodeLink: "https://github.com/shraddhesh-jadhav-007/personal-portfolio-website.git",
   },
   
